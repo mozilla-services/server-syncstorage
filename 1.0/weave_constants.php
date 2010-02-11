781b666cea1dfc9a2f644ce65bcd0220d4d1d4eb
Toby Elliott <telliott@mozilla.com>
2010-02-11 12:09:44 -0800

    sanitizing the http hosts in case allow_url_fopen is on.

--- 1.0/weave_constants.php
+++ 1.0/weave_constants.php
@@ -1,7 +1,11 @@
 <?php
-	if (array_key_exists('HTTP_HOST', $_SERVER) && file_exists($_SERVER['HTTP_HOST'] . '_constants.php'))
-		require_once $_SERVER['HTTP_HOST'] . '_constants.php';
-
+	if (array_key_exists('HTTP_HOST', $_SERVER))
+	{
+		$host = preg_replace('/[^a-z0-9\.\-]/i', '', $_SERVER['HTTP_HOST']);
+		if (file_exists($host . '_constants.php'))
+			require_once $_SERVER['HTTP_HOST'] . '_constants.php';
+	}
+	
 	if (file_exists('default_constants.php'))
 		require_once 'default_constants.php';
 
