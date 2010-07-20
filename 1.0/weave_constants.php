2bbb520a186341e132a76aa6a5d3de4818e0d093
Toby Elliott <telliott@mozilla.com>
2010-07-20 16:34:23 -0700

    removing all the non-python stuff

--- 1.0/weave_constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if (file_exists('shard_constants.php'))
-			require_once 'shard_constants.php';
-	
-	if (file_exists('default_constants.php'))
-		require_once 'default_constants.php';
-
-
-?>
