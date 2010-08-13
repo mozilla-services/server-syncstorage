31f80ab0fce8b49ccef631307bc5307aa6adcf6e
Tarek Ziade <tarek@ziade.org>
2010-08-13 14:04:06 +0200

    started to merg with Toby implementation

--- 1.1/weaveserver/log.py
+++ 1.1/weaveserver/log.py
@@ -36,60 +36,72 @@
 import socket
 import logging
 from time import strftime
-
+import re
 
 CEF = 60
 logging.addLevelName('CEF', CEF)
 
 _HOST = socket.gethostname()
-_CEF_AUTH_FAILURE = 'AuthFail'
-_CEF_CAPTCHA_FAILURE = 'CaptchaFail'
-_CEF_OVERRIDE_FAILURE = 'InvalidAdmin'
-_CEF_ACCOUNT_LOCKED = 'AccountLockout'
-_CEF_FORMAT = ('%(date)s %(host)s CEF:%(version)s|%(vendor)s|%(product)s|'
-               '%(device_version)s|%(signature)s|%(name)s|%(severity)s|'
-               'cs1Label=requestClientApplication cs1=%(user_agent)s '
-               'requestMethod=%(method)s request=%(url)s '
-               'src=%(source)s dest=%(dest)s')
+_CEF_AUTH_FAILURE = u'AuthFail'
+_CEF_CAPTCHA_FAILURE = u'CaptchaFail'
+_CEF_OVERRIDE_FAILURE = u'InvalidAdmin'
+_CEF_ACCOUNT_LOCKED = u'AccountLockout'
+_CEF_FORMAT = (u'%(date)s %(host)s CEF:%(version)s|%(vendor)s|%(product)s|'
+                '%(device_version)s|%(signature)s|%(name)s|%(severity)s|'
+                'cs1Label=requestClientApplication cs1=%(user_agent)s '
+                'requestMethod=%(method)s request=%(url)s '
+                'src=%(source)s dest=%(dest)s')
+
+_FIND_PIPE = re.compile(r'([|\\=])')
 
 
 class WeaveLogger(logging.getLoggerClass()):
     """Custom logger that adds CEF-specific logs methods"""
 
     def _convert(self, data):
-        """Escapes |"""
-        return str(data).replace('|', r'\|')
+        """Escapes | and = and convert to unicode"""
+        if not isinstance(data, unicode):
+            if isinstance(data, str):
+                data = unicode(data, 'utf8')
+            else:
+                data = unicode(data)
 
-    def cef_auth_failure(self, message, severity, request):
+        return _FIND_PIPE.sub(r'\\\1', data)
+
+    def cef_auth_failure(self, message, severity, request, **kw):
         """Creates a CEF record, and emit it."""
         signature = self._convert(_CEF_AUTH_FAILURE)
         name = self._convert(message)
         severity = self._convert(severity)
 
-        data = {'severity': severity,
-                'source': request.remote_addr,
-                'method': request.method,
-                'url': request.url,
-                'dest': request.host,
-                'user_agent': request.headers.get('User-Agent', 'none'),
-                'signature': signature,
-                'name': name,
-                'version': 0,
-                'vendor': 'mozilla',
-                'device_version': '1.3',
-                'product': 'weave',
-                'host': _HOST,
-                'date': strftime("%b %d %H:%M:%S")}
+        kw.update({'severity': severity,
+                   'source': request.remote_addr,
+                   'method': request.method,
+                   'url': request.url,
+                   'dest': request.host,
+                   'user_agent': request.headers.get('User-Agent', u'none'),
+                   'signature': signature,
+                   'name': name,
+                   'version': '0',
+                   'vendor': u'mozilla',
+                   'device_version': u'1.3',
+                   'product': u'weave',
+                   'host': _HOST,
+                   'date': strftime("%b %d %H:%M:%S")})
 
         # make sure we don't have a | anymore
-        for key, value in data.items():
-            value = str(value)
-            pos = value.find('|')
-            if pos == 0 or (pos > 0 and value[pos - 1] != '\\'):
+        for key, value in kw.items():
+            if not isinstance(value, unicode):
+                if isinstance(value, str):
+                    value = unicode(value, 'utf8')
+                else:
+                    value = unicode(value)
+
+            if _FIND_PIPE.match(value):
                 msg = '"%s" cannot contain a "|" char: "%s"' % (key, value)
                 raise ValueError(msg)
 
-        self.log(CEF, _CEF_FORMAT, data)
+        self.log(CEF, _CEF_FORMAT, kw)
 
 logging.setLoggerClass(WeaveLogger)
 logger = logging.getLogger('weaveserver')
