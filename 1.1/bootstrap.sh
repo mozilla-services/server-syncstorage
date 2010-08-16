a74edf07e8888a80044c450682e483d3a072cd75
Tarek Ziade <tarek@ziade.org>
2010-08-16 13:42:23 +0200

    added flake8 to the dev environment

--- 1.1/bootstrap.sh
+++ 1.1/bootstrap.sh
@@ -1,16 +1,18 @@
+#!/bin/sh
 if [ ! -d 'bin' ]; then
   virtualenv > /dev/null
   if [ $? -eq 0 ]; then
     wget http://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.4.9.tar.gz
-    ar -xzvf virtualenv-1.4.9.tar.gz
+    tar -xzvf virtualenv-1.4.9.tar.gz
     cd virtualenv-1.4.9
-    sudo python2.6 setup.py install
+    python2.6 setup.py install
     cd ..
     rm -rf virtualenv*
   fi
   virtualenv --no-site-packages .
   bin/easy_install nose
   bin/easy_install coverage
+  bin/easy_install flake8
   bin/python setup.py develop
 fi
 
