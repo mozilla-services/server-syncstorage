01ca7557fa1e077bcfc463c2131cd083e0b2f2ca
Rob Miller <rmiller@mozilla.com>
2011-07-26 17:33:54 -0700

    bug 672642 - convert static SQL queries to raw SQL - r=tarek,telliott

--- syncstorage/storage/queries.py
+++ syncstorage/storage/queries.py
@@ -36,96 +36,89 @@
 """
 Pre-built queries
 """
-from syncstorage.storage.sqlmappers import (users, collections, wbo,
-                                            get_wbo_table)
-from sqlalchemy.sql import select, bindparam, delete, func, and_
-
+from syncstorage.storage.sqlmappers import collections, wbo, get_wbo_table
+from sqlalchemy.sql import select, bindparam, delete, and_, text
 
 _USER_N_COLL = and_(collections.c.userid == bindparam('user_id'),
                     collections.c.name == bindparam('collection_name'))
+
+queries = {
+    'USER_EXISTS': 'SELECT id FROM users where id = :user_id',
+
+    'DELETE_SOME_USER_WBO': 'DELETE FROM %(wbo)s WHERE username=:user_id AND '
+                            'collection=:collection_id AND id=:item_id',
+
+    'DELETE_USER_COLLECTIONS': 'DELETE FROM collections WHERE '
+                               'userid=:user_id',
+
+    'DELETE_USER_COLLECTION': delete(collections).where(_USER_N_COLL),
+
+    'DELETE_USER_WBOS': 'DELETE FROM %(wbo)s WHERE username=:user_id',
+
+    'DELETE_USER': 'DELETE FROM users WHERE id=:user_id',
+
+    'COLLECTION_EXISTS': select([collections.c.collectionid], _USER_N_COLL),
+
+    'COLLECTION_NEXTID': 'SELECT MAX(collectionid) FROM collections '
+                         'WHERE userid=:user_id',
+
+    'COLLECTION_MODIFIED': 'SELECT wbo_a.modified FROM %(wbo)s AS wbo_a, '
+                           '%(wbo)s WHERE wbo_a.username=%(wbo)s.username '
+                           'AND wbo_a.collection=%(wbo)s.collection '
+                           'ORDER BY wbo_a.username DESC, '
+                           'wbo_a.collection DESC, wbo_a.modified DESC '
+                           'LIMIT 1',
+
+    'COLLECTION_STAMPS': 'SELECT collection, MAX(modified) FROM %(wbo)s '
+                         'WHERE username=:user_id GROUP BY username, '
+                         'collection',
+
+    'COLLECTION_COUNTS': 'SELECT collection, COUNT(collection) FROM %(wbo)s '
+                         'WHERE username=:user_id AND ttl>:ttl '
+                         'GROUP BY collection',
+
+    'COLLECTION_MAX_STAMPS': 'SELECT MAX(modified) FROM %(wbo)s WHERE '
+                             'collection=:collection_id AND '
+                             'username=:user_id',
+
+    'ITEM_EXISTS': 'SELECT modified FROM %(wbo)s WHERE '
+                   'collection=:collection_id AND username=:user_id '
+                   'AND id=:item_id AND ttl>:ttl',
+
+    'DELETE_ITEMS': 'DELETE FROM %(wbo)s WHERE collection=:collection_id AND '
+                    'username=:user_id AND ttl>:ttl',
+
+    'USER_STORAGE_SIZE': 'SELECT SUM(payload_size) FROM %(wbo)s WHERE '
+                         'username=:user_id AND ttl>:ttl',
+
+    'COLLECTIONS_STORAGE_SIZE': 'SELECT collection, SUM(payload_size) '
+                                'FROM %(wbo)s WHERE username=:user_id AND '
+                                'ttl>:ttl GROUP BY collection',
+
+    'USER_COLLECTION_NAMES': 'SELECT collectionid, name FROM collections '
+                             'WHERE userid=:user_id',
+    }
 
 
 def get_query(name, user_id=None):
     if user_id is None:
         table = wbo
-        wbo_alias = wbo.alias()
     else:
         table = get_wbo_table(user_id)
 
-    wbo_alias = table.alias()
+    queries['ITEM_ID_COL_USER'] = and_(
+        table.c.collection == bindparam('collection_id'),
+        table.c.username == bindparam('user_id'),
+        table.c.id == bindparam('item_id'),
+        table.c.ttl > bindparam('ttl'))
 
-    if name == 'USER_EXISTS':
-        return select([users.c.id], users.c.id == bindparam('user_id'))
-    elif name == 'DELETE_SOME_USER_WBO':
-        return delete(table).where( \
-                                 and_(table.c.username == bindparam('user_id'),
-                                      table.c.collection ==
-                                      bindparam('collection_id'),
-                                      table.c.id == bindparam('item_id')))
-    elif name == 'DELETE_USER_COLLECTIONS':
-        return delete(collections).where( \
-                                 collections.c.userid == bindparam('user_id'))
-    elif name == 'DELETE_USER_COLLECTION':
-        return delete(collections).where(_USER_N_COLL)
-    elif name == 'DELETE_USER_WBOS':
-        return delete(table, table.c.username == bindparam('user_id'))
-    elif name == 'DELETE_USER':
-        return delete(users, users.c.id == bindparam('user_id'))
-    elif name == 'COLLECTION_EXISTS':
-        return select([collections.c.collectionid], _USER_N_COLL)
-    elif name == 'COLLECTION_NEXTID':
-        return select([func.max(collections.c.collectionid)],
-                      collections.c.userid == bindparam('user_id'))
-    elif name == 'COLLECTION_MODIFIED':
-        return select([wbo_alias.c.modified],
-              and_(wbo_alias.c.username == table.c.username,
-                   wbo_alias.c.collection == table.c.collection)).\
-              order_by(wbo_alias.c.username.desc(),
-                       wbo_alias.c.collection.desc(),
-                       wbo_alias.c.modified.desc()).limit(1).as_scalar()
-    elif name == 'COLLECTION_STAMPS':
-        return select([table.c.collection, func.max(table.c.modified)],
-                      table.c.username ==
-                        bindparam('user_id')).group_by(table.c.username,
-                                                       table.c.collection)
-    elif name == 'COLLECTION_COUNTS':
-        return select([table.c.collection, func.count(table.c.collection)],
-           and_(table.c.username == bindparam('user_id'),
-                table.c.ttl > bindparam('ttl'))).group_by(table.c.collection)
-    elif name == 'COLLECTION_MAX_STAMPS':
-        return select([func.max(table.c.modified)],
-            and_(table.c.collection == bindparam('collection_id'),
-                 table.c.username == bindparam('user_id')))
-    elif name == 'ITEM_ID_COL_USER':
-        return and_(table.c.collection == bindparam('collection_id'),
-                    table.c.username == bindparam('user_id'),
-                    table.c.id == bindparam('item_id'),
-                    table.c.ttl > bindparam('ttl'))
+    query = queries.get(name)
+    if query is None:
+        raise ValueError(name)
 
-    elif name == 'ITEM_EXISTS':
+    if isinstance(query, str):
+        if '%(wbo)s' in query:
+            query = query % {'wbo': table.name}
+        query = text(query)
 
-        col_user = and_(table.c.collection == bindparam('collection_id'),
-                         table.c.username == bindparam('user_id'),
-                         table.c.id == bindparam('item_id'),
-                         table.c.ttl > bindparam('ttl'))
-        return select([table.c.modified], col_user)
-    elif name == 'DELETE_ITEMS':
-        return delete(table,
-                       and_(table.c.collection == bindparam('collection_id'),
-                            table.c.username == bindparam('user_id'),
-                            table.c.ttl > bindparam('ttl')))
-    elif name == 'USER_STORAGE_SIZE':
-        return select([func.sum(table.c.payload_size)],
-                       and_(table.c.username == bindparam('user_id'),
-                            table.c.ttl > bindparam('ttl')))
-    elif name == 'COLLECTIONS_STORAGE_SIZE':
-        return select([table.c.collection,
-            func.sum(table.c.payload_size)],
-            and_(table.c.username == bindparam('user_id'),
-                 table.c.ttl > bindparam('ttl'))).group_by(table.c.collection)
-    elif name == 'USER_COLLECTION_NAMES':
-        return select([collections.c.collectionid,
-                       collections.c.name],
-                      collections.c.userid == bindparam('user_id'))
-
-    raise ValueError(name)
+    return query
