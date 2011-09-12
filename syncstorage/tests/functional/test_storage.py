8b38fa1da03a9b60fa14b44a6c0b5bd748d6d821
Ryan Kelly <ryan@rfk.id.au>
2011-09-13 09:51:37 +1000

    Bug 685384 - remove ref to unimplemented/obsolete 'depth' parameter; r=tarek

--- syncstorage/tests/functional/test_storage.py
+++ syncstorage/tests/functional/test_storage.py
@@ -540,7 +540,6 @@ class TestStorage(support.TestWsgiApp):
         #   'oldest' - Orders by modification date (oldest first)
         #   'newest' - Orders by modification date (newest first)
         #   'index' - Orders by the sortindex (ordered lists)
-        #   'depthindex' - Orders by depth, then by sortindex (ordered trees)
 
         # sort is used only if limit is used.
         # check this with toby
