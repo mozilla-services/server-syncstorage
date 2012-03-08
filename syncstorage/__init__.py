ce5397c0d09a4df07bef870f3e71e5d9d6c77d04
Rob Miller <rob@kalistra.com>
2012-03-07 17:15:42 -0800

    use load_from_settings instead of manual metlog setup

--- syncstorage/__init__.py
+++ syncstorage/__init__.py
@@ -3,7 +3,8 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from mozsvc.config import get_configurator
-from mozsvc.metrics import setup_metlog, get_metlog_client
+from mozsvc.plugin import load_from_settings
+from mozsvc.metrics import get_metlog_client
 from syncstorage.controller import StorageController
 
 logger = get_metlog_client()
@@ -26,6 +27,6 @@ def includeme(config):
 
 def main(global_config, **settings):
     config = get_configurator(global_config, **settings)
+    load_from_settings('metlog', config.registry.settings)
     config.include(includeme)
-    setup_metlog(config.registry.settings.getsection('metlog'))
     return config.make_wsgi_app()
