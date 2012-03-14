3e3d888261a44767895901e2080dd4b6a48e74b5
Rob Miller <rob@kalistra.com>
2012-03-14 12:30:45 -0700

    add _make_request, and no mo' monkey

--- syncstorage/tests/test_metlog.py
+++ syncstorage/tests/test_metlog.py
@@ -2,9 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 from metlog.senders import DebugCaptureSender
-from mozsvc.metrics import get_metlog_client, monkey_service_class
+from mozsvc.metrics import get_metlog_client
 from mozsvc.metrics import setup_metlog, teardown_metlog
-from mozsvc.tests.support import get_test_configurator
+from mozsvc.tests.support import get_test_configurator, make_request
 import unittest
 
 
@@ -13,14 +13,17 @@ class TestMetlog(unittest.TestCase):
         self.config = get_test_configurator(__file__)
         self.config.include('syncstorage')
         setup_metlog(self.config.registry.settings.getsection('metlog'))
-        monkey_service_class()
         self.client = get_metlog_client()
 
     def tearDown(self):
         teardown_metlog()
-        monkey_service_class(un=True)
 
-    def test_metlog_setup(self):
+    def _make_request(self, *args, **kwds):
+        req = make_request(self.config, *args, **kwds)
+        req.user = {'userid': 'aa'}
+        return req
+
+    def test_sender_class(self):
         sender = self.client.sender
         self.assertTrue(isinstance(sender, DebugCaptureSender))
 
