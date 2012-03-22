0ed7cfd2ba9084e6baab22915140c506590078bc
Rob Miller <rob@kalistra.com>
2012-03-22 11:59:12 -0700

    add test for metlog view wrappers

--- syncstorage/tests/test_metlog.py
+++ syncstorage/tests/test_metlog.py
@@ -5,6 +5,7 @@ from metlog.senders import DebugCaptureSender
 from mozsvc.metrics import get_metlog_client
 from mozsvc.metrics import setup_metlog, teardown_metlog
 from mozsvc.tests.support import get_test_configurator, make_request
+import json
 import unittest
 
 
@@ -27,3 +28,18 @@ class TestMetlog(unittest.TestCase):
         sender = self.client.sender
         self.assertTrue(isinstance(sender, DebugCaptureSender))
 
+    def test_service_view_wrappers(self):
+        from syncstorage.views import get_info_counts
+        req = self._make_request(environ={"HTTP_HOST": "localhost"})
+        get_info_counts(req)
+        msgs = self.client.sender.msgs
+        self.assertEqual(len(msgs), 2)
+        timer_msg = json.loads(msgs[0])
+        wsgi_msg = json.loads(msgs[1])
+        self.assertEqual(timer_msg['type'], 'timer')
+        self.assertEqual(timer_msg['fields']['name'],
+                         'syncstorage.views:get_info_counts')
+        self.assertEqual(wsgi_msg['type'], 'wsgi')
+        self.assertEqual(wsgi_msg['fields']['headers'],
+                         {'path': '/', 'host': 'localhost',
+                          'User-Agent': ''})
