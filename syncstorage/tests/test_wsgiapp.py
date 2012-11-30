2045d3ce4183130a8130f80272ef8ac45f1cbb18
Ryan Kelly <rfkelly@mozilla.com>
2012-11-30 15:55:41 +1100

    DO NOT MERGE; Bug 750964 pt2 - check node health status in memcache; r=XXXXXX

--- syncstorage/tests/test_wsgiapp.py
+++ syncstorage/tests/test_wsgiapp.py
@@ -2,9 +2,24 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from webtest import TestApp
+
 from syncstorage import main, tweens
 from syncstorage.storage import get_storage
 from syncstorage.tests.support import StorageTestCase
+
+
+class FakeMemcachedClient(object):
+    """A simple in-memory fake for the MemcachedClient class."""
+
+    def __init__(self):
+        self.values = {}
+
+    def get(self, key):
+        return self.values.get(key, None)
+
+    def set(self, key, value):
+        self.values[key] = value
 
 
 class TestWSGIApp(StorageTestCase):
@@ -30,12 +45,77 @@ class TestWSGIApp(StorageTestCase):
 
     def test_dependant_options(self):
         # make sure the app cannot be initialized if it's asked
-        # to check for blacklisted node and memcached is not present
+        # to check for node status and memcached is not present
         settings = self.config.registry.settings.copy()
-        settings['storage.check_blacklisted_nodes'] = True
-        old_client = tweens.Client
-        tweens.Client = None
+        settings['storage.check_node_status'] = True
+        old_client = tweens.MemcachedClient
+        tweens.MemcachedClient = None
         try:
             self.assertRaises(ValueError, main, {}, **settings)
         finally:
-            tweens.Client = old_client
+            tweens.MemcachedClient = old_client
+
+    def test_checking_node_status_in_memcache(self):
+        config = self.config
+
+        app = TestApp(config.make_wsgi_app(), extra_environ={
+            "HTTP_HOST": "some-test-host",
+        })
+
+        cache = FakeMemcachedClient()
+        config.registry["check_node_status:cache"] = cache
+
+        # The app automatically fuzzes the backoff value in headers, so
+        # we can't use a straight assertEquals to check it.
+        def assertHasBackoffHeader(response, header="X-Backoff", value=None):
+            if value is None:
+                value = config.registry.settings["mozsvc.retry_after"]
+            got_value = int(response.headers[header])
+            if not value <= got_value <= value + 5:
+                msg = "Backoff of %d does not match %d" % (got_value, value)
+                assert False, msg
+
+        # With no node data in memcache, requests to known nodes should
+        # succeed while requests to unknown nodes should fail.
+        app.get("/__heartbeat__", headers={"Host": "some-test-host"},
+                status=200)
+        app.get("/__heartbeat__", headers={"Host": "unknown-host"},
+                status=503)
+
+        # Marking the node as "backoff" will succeed, but send backoff header.
+        cache.set("status:some-test-host", "backoff")
+        r = app.get("/__heartbeat__", status=200)
+        assertHasBackoffHeader(r, "X-Backoff")
+
+        cache.set("status:some-test-host", "backoff:100")
+        r = app.get("/__heartbeat__", status=200)
+        assertHasBackoffHeader(r, "X-Backoff", 100)
+
+        # Marking the node as "down", "draining" or "unhealthy" will result
+        # in a 503 response with backoff header.
+        cache.set("status:some-test-host", "down")
+        r = app.get("/__heartbeat__", status=503)
+        assertHasBackoffHeader(r, "X-Backoff")
+        assertHasBackoffHeader(r, "Retry-After")
+
+        cache.set("status:some-test-host", "draining")
+        r = app.get("/__heartbeat__", status=503)
+        assertHasBackoffHeader(r, "X-Backoff")
+        assertHasBackoffHeader(r, "Retry-After")
+
+        cache.set("status:some-test-host", "unhealthy")
+        r = app.get("/__heartbeat__", status=503)
+        assertHasBackoffHeader(r, "X-Backoff")
+        assertHasBackoffHeader(r, "Retry-After")
+
+        # A nonsense node status will be ignored.
+        cache.set("status:some-test-host", "nonsensical-value")
+        r = app.get("/__heartbeat__", status=200)
+        self.assertTrue("X-Backoff" not in r.headers)
+
+        # Node status only affects the node that it belongs to.
+        cache.set("status:some-test-host", "unhealthy")
+        r = app.get("/__heartbeat__",
+                    headers={"Host": "another-test-host"},
+                    status=200)
+        self.assertTrue("X-Backoff" not in r.headers)
--- syncstorage/tweens.py
+++ syncstorage/tweens.py
@@ -9,9 +9,9 @@ from pyramid.httpexceptions import HTTPException, HTTPServiceUnavailable
 
 
 try:
-    from memcache import Client
+    from mozsvc.storage.mcclient import MemcachedClient
 except ImportError:
-    Client = None       # NOQA
+    MemcachedClient = None       # NOQA
 
 
 def set_x_timestamp_header(handler, registry):
@@ -26,36 +26,104 @@ def set_x_timestamp_header(handler, registry):
     return set_x_timestamp_header_tween
 
 
-def check_for_blacklisted_nodes(handler, registry):
-    """Tween to check for blacklisted nodes by querying memcache."""
+def check_node_status(handler, registry):
+    """Tween to check storage node status by querying memcache.
 
-    # Check whether this tween is enabled in the config.
-    if not registry.settings.get("storage.check_blacklisted_nodes", False):
-        return handler
+    If configured to do so, this function can check memcache for the status
+    of the target node and possibly avoid calling out to the storage backend.
+    It looks for a memcache key named "status:<hostname>" with one of the
+    following values:
 
-    # Create a memcached client, error out if that's not possible.
-    if Client is None:
-        raise ValueError('The "check_blacklisted_nodes" option '
-                         'requires a memcached server')
+       "down":   the node is explicitly marked as down
+       "draining":   the node is being decommissioned
+       "unhealthy":  the node has not responded to health checks
+       "backoff" or "backoff:NN":  the node is under heavy load and
+                                   clients should back off for a while.
 
-    servers = registry.settings.get('storage.cache_servers', '127.0.0.1:11211')
-    cache = registry["cache"] = Client(servers.split(','))
+    """
 
-    def check_for_blacklisted_nodes_tween(request):
-        # Is it down?  We just error out straight away.
-        host = request.host
-        if cache.get("down:" + host) is not None:
-            raise HTTPServiceUnavailable("Server Problem Detected")
+    # Check whether we're required to perform the checks.
+    # If so then we must be able to create a memcached client.
+    if not registry.settings.get("storage.check_node_status", False):
+        registry["check_node_status:cache"] = None
+    else:
+        if MemcachedClient is None:
+            msg = 'The "check_node_status" option requires a memcached server'
+            raise ValueError(msg)
+        servers = registry.settings.get('storage.cache_servers')
+        if servers is None:
+            servers = ["127.0.0.1:11211"]
+        else:
+            servers = servers.split(",")
+        registry["check_node_status:cache"] = MemcachedClient(servers)
 
+    def check_node_status_tween(request):
+
+        # Don't perform the check unless configured to do so.
+        cache = request.registry.get("check_node_status:cache")
+        if cache is None:
+            return handler(request)
+
+        headers = {}
+        response = None
+        settings = request.registry.settings
+
+        # A helper function to create HTTPServiceUnavailable responses,
+        # while providing all the necessary headers etc.
+        def resp_service_unavailable(msg):
+            retry_after = str(settings.get("mozsvc.retry_after", 1800))
+            headers.setdefault("Retry-After", retry_after)
+            headers.setdefault("X-Backoff", retry_after)
+            return HTTPServiceUnavailable(body=msg, headers=headers)
+
+        # Get the node name from the host header,
+        # and check that it's one we know about.
+        node = request.host
+        if not node:
+            msg = "host header not received from client"
+            return resp_service_unavailable(msg)
+
+        if "syncstorage:storage:host:" + node not in request.registry:
+            msg = "database lookup failed"
+            return resp_service_unavailable(msg)
+
+        # Check the node's status in memcache,
+        # and return early with an error if it's not as expected.
+        status = cache.get('status:%s' % (node,))
+        if status is not None:
+
+            # If it's marked as draining then send a 503 response.
+            # XXX TODO: consider sending a 401 to trigger migration?
+            if status == "draining":
+                msg = "node reassignment"
+                return resp_service_unavailable(msg)
+
+            # If it's marked as being down then send a 503 response.
+            if status == "down":
+                msg = "database marked as down"
+                return resp_service_unavailable(msg)
+
+            # If it's marked as being unhealthy then send a 503 response.
+            if status == "unhealthy":
+                msg = "database is not healthy"
+                return resp_service_unavailable(msg)
+
+            # If it's marked for backoff, proceed with the request
+            # but set appropriate headers on the response.
+            if status == "backoff" or status.startswith("backoff:"):
+                try:
+                    retry_after = status.split(":", 1)[1]
+                except IndexError:
+                    retry_after = str(settings.get("mozsvc.retry_after", 1800))
+                headers["X-Backoff"] = retry_after
+
+        # If we get to here then we can proceed with contacting the storage
+        # note, but may have some control headers to add to the response.
         response = handler(request)
-
-        # Is it backed-off?  We process the request but add a header.
-        backoff = cache.get('backoff:%s' % host)
-        if backoff is not None:
-            response.headers["X-Backoff"] = str(backoff)
+        response.headers.update(headers)
         return response
 
-    return check_for_blacklisted_nodes_tween
+    return check_node_status_tween
 
 
 def set_default_accept_header(handler, registry):
@@ -100,7 +168,7 @@ def fuzz_retry_after_header(handler, registry):
 
 def includeme(config):
     """Include all the SyncServer tweens into the given config."""
-    config.add_tween("syncstorage.tweens.check_for_blacklisted_nodes")
+    config.add_tween("syncstorage.tweens.check_node_status")
     config.add_tween("syncstorage.tweens.set_x_timestamp_header")
     config.add_tween("syncstorage.tweens.set_default_accept_header")
     config.add_tween("syncstorage.tweens.fuzz_retry_after_header")
