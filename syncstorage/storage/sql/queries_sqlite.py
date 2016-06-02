ca121df4250d8242faf199967ca36431bb5d684c
Ryan Tilder <ryan@tilder.org>
2016-06-02 15:16:02 -0700

    Significantly less broken SQLite specific upsert-esque query

--- syncstorage/storage/sql/queries_sqlite.py
+++ syncstorage/storage/sql/queries_sqlite.py
@@ -23,24 +23,26 @@ LOCK_COLLECTION_WRITE = "SELECT last_modified FROM user_collections "\
 PURGE_SOME_EXPIRED_ITEMS = "DELETE FROM %(bso)s "\
                            "WHERE ttl < (strftime('%%s', 'now') - :grace) "
 
-APPLY_BATCH = "INSERT OR REPLACE INTO %(bso)s " \
-                     "  (userid, collection, id, modified, sortindex, " \
-                     "   payload, payload_size, ttl) " \
-                     "SELECT userid, collection, id, modified, " \
-                     "  sortindex, COALESCE(payload, \"\"), " \
-                     "  COALESCE(payload_size, 0), " \
-                     "  COALESCE(ttl, default_ttl) " \
-                     "FROM ( " \
-                     "  SELECT batch, userid, collection, " \
-                     "         :modified AS modified, " \
-                     "         :default_ttl AS default_ttl " \
-                     "  FROM batch_uploads WHERE batch = :batch "\
-                     ") LEFT JOIN ( " \
-                     "  SELECT id, sortindex, payload, " \
-                     "         payload_size, ttl " \
-                     "  FROM %(bui)s " \
-                     ") ON batch"
-
+APPLY_BATCH = "INSERT OR REPLACE INTO %(bso)s" \
+              "    (userid, collection, id, sortindex, payload," \
+              "     payload_size, ttl, modified)" \
+              "  SELECT booey.userid, booey.collection, booey.id," \
+              "         :modified," \
+              "         COALESCE(booey.sortindex, %(bso)s.sortindex)," \
+              "         COALESCE(booey.payload, %(bso)s.payload, "")," \
+              "         COALESCE(booey.payload_size, %(bso)s.payload_size," \
+              "                  0)," \
+              "         COALESCE(booey.ttl, %(bso)s.ttl, 2100000000)" \
+              "  FROM (SELECT batch_uploads.batch, batch_uploads.userid," \
+              "               batch_uploads.collection, %(bui)s.id," \
+              "               sortindex, payload, payload_size, ttl" \
+              "        FROM %(bui)s" \
+              "        LEFT JOIN batch_uploads" \
+              "        ON %(bui)s.batch = batch_uploads.batch" \
+              "        WHERE %(bui)s.batch = :batch) AS booey" \
+              "  LEFT JOIN %(bso)s ON booey.userid = %(bso)s.userid AND" \
+              "                   booey.collection = %(bso)s.collection AND" \
+              "                   booey.id = %(bso)s.id"
 
 PURGE_SOME_EXPIRED_ITEMS = "DELETE FROM %(bso)s WHERE ttl < " \
                            "(SELECT strftime('%%s', 'now') - :grace)"
