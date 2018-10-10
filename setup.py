6f103c2d8f137b3ff9f353885f9a47d6a9cf9414
Niko Wenselowski <der@nik0.de>
2018-10-10 09:05:18 +0200

    Install memcache extras from mzsvc to have a memcache client.

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from setuptools import setup, find_packages
-import os
-import re
 
 install_requires = ['SQLALchemy', 'unittest2', 'simplejson', 'pyramid',
-                    'mozsvc>=0.8', 'cornice', 'pyramid_hawkauth', 'PyMySQL',
-                    'pymysql_sa', 'umemcache', 'wsgiproxy',
-                    'webtest', 'requests', 'PyBrowserID', 'testfixtures']
+                    'mozsvc[memcache]>=0.8', 'cornice', 'pyramid_hawkauth',
+                    'PyMySQL', 'pymysql_sa', 'wsgiproxy', 'webtest',
+                    'requests', 'PyBrowserID', 'testfixtures']
 
 entry_points = """
 [paste.app_factory]
@@ -26,11 +24,10 @@ setup(name='SyncStorage',
       entry_points=entry_points,
       license='MPLv2.0',
       classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-        ],
+          "Programming Language :: Python",
+          "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+      ],
       package_data={
           "syncstorage.tests": ["*.ini"],
       },
-      include_package_data=True,
-)
+      include_package_data=True)
